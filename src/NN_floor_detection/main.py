fefac15b9e5de6659200433b09687bd4385f997c
h4vlik <havlik.hav@gmail.com>
2021-01-15 10:07:36 +0100

    atomatic testing of models

--- src/NN_floor_detection/main.py
+++ src/NN_floor_detection/main.py
@@ -3,7 +3,7 @@ Main
 """
 
 from core.nn_model import build_model
-from core.dataset import generate_dataset
+from core.dataset import generate_dataset, generate_test_dataset
 
 import matplotlib.pyplot as plt
 from PIL import ImageFile
@@ -24,6 +24,8 @@ variables
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 # path to train folder
 train_folder_path = Path("data/Dataset_try/")
+# path to train folder
+test_folder_path = Path("data/Dataset_ready/")
 # path for saving model
 model_path = ".data/model_save"
 # input image size
@@ -97,6 +99,46 @@ def predict_image(model, PATH_TO_IMG, image_size):
 
     print("Labels: ", result)
 
+
+def predict_img_loop(model, path_read, image_size=(32, 32)):
+    # go trough all folders in the path
+    for fo_number, folder in enumerate(os.listdir(path_read)):
+        print("new folder")
+        for fi_number, filename in enumerate(os.listdir(path_read+'\\'+folder)):
+            if fi_number > 20:
+                break
+            else:
+                img_path = path_read+str(folder)+'\\'+str(filename)
+                img = keras.preprocessing.image.load_img(
+                    img_path,
+                    target_size=image_size,
+                    color_mode="rgb",
+                    grayscale=False
+                )
+
+                img_array = keras.preprocessing.image.img_to_array(img)
+
+                tf_img_array = tf.expand_dims(img_array, 0)  # Create batch axis
+
+                predictions = model.predict(tf_img_array)
+                result = CLASS_NAMES[predictions.argmax(axis=1)[0]]
+
+                print(
+                    "Predictions: ", predictions
+                )
+
+                plt.imshow(img_array/255.0)
+                plt.title(result)
+                plt.show()
+
+                print("Labels: ", result)
+
+
+def predict_dataset(model, IMG_SIZE):
+    test_ds = generate_test_dataset(test_folder_path, image_size=IMG_SIZE)
+    print("Evaluate")
+    result = model.evaluate(test_ds)
+
 # TODO: create function for training with saved weights
 
 
@@ -109,6 +151,7 @@ if __name__ == "__main__":
     PATH_TO_IMG = Path("data/test/img_test/6_frame_014762.jpeg")
     PATH_TO_WEIGHS = Path("results/training_checkpoints/")
     PATH_TO_MODEL = "data/models/svhn_best_cnn.h5"
+    path_read = ('data/Dataset_ready/')
 
     # train = True --> train neural network
     # train = False --> load model or weights and eveluate network
@@ -127,4 +170,7 @@ if __name__ == "__main__":
         else:
             model_old = build_model(NUM_CLASSES)
             model = load_w(model_old, PATH_TO_WEIGHS)
-        predict_image(model, PATH_TO_IMG, IMG_SIZE)
+
+        # predict_image(model, PATH_TO_IMG, IMG_SIZE)
+        # predict_dataset(model, IMG_SIZE)
+        predict_img_loop(model, path_read)
