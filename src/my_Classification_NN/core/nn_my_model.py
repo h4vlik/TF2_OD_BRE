62f7e8948224e1756fda39576e853454c928158f
h4vlik <havlik.hav@gmail.com>
2021-01-17 23:33:51 +0100

    everything works - but no well

--- src/my_Classification_NN/core/nn_my_model.py
+++ src/my_Classification_NN/core/nn_my_model.py
@@ -1,3 +1,5 @@
+"""Core nn script, HAVELKA 15/01/2021
+"""
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
@@ -6,13 +8,99 @@ import matplotlib.pyplot as plt
 from pathlib import Path
 
 
-def build_my_model(num_classes, input_shape):
+def build_my_model(input_shape, NUM_CLASSES=10):
     """
     build_my_model
     - build model from scratch
-    - compile
 
     :param NUM_CLASSES: number of classes to be trained
 
     :return model: model_my, keras model variable
     """
+
+    model = keras.Sequential([
+        keras.Input(shape=input_shape),
+        layers.Conv2D(32, (3, 3), padding='same', activation='relu'),
+        layers.BatchNormalization(),
+
+        layers.Conv2D(32, (3, 3), padding='same', activation='relu'),
+        layers.MaxPooling2D((2, 2)),
+        layers.Dropout(0.3),
+
+        layers.Conv2D(64, (3, 3), padding='same', activation='relu'),
+        layers.BatchNormalization(),
+
+        layers.Conv2D(64, (3, 3), padding='same', activation='relu'),
+        layers.MaxPooling2D((2, 2)),
+        layers.Dropout(0.3),
+
+        layers.Conv2D(128, (3, 3), padding='same', activation='relu'),
+        layers.BatchNormalization(),
+
+        layers.Conv2D(128, (3, 3), padding='same', activation='relu'),
+        layers.MaxPooling2D((2, 2)),
+        layers.Dropout(0.3),
+
+        layers.Flatten(),
+        layers.Dense(128, activation='relu'),
+        layers.Dropout(0.4),
+        layers.Dense(NUM_CLASSES,  activation='softmax')
+    ])
+
+    return model
+
+
+def make_model(input_shape, NUM_CLASSES=10):
+    inputs = keras.Input(shape=input_shape)
+    # Entry block
+    x = layers.Conv2D(32, 3, strides=2, padding="same")(inputs)
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation("relu")(x)
+
+    x = layers.Conv2D(64, 3, padding="same")(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation("relu")(x)
+
+    previous_block_activation = x  # Set aside residual
+
+    for size in [128, 256, 512, 728]:
+        x = layers.Activation("relu")(x)
+        x = layers.SeparableConv2D(size, 3, padding="same")(x)
+        x = layers.BatchNormalization()(x)
+
+        x = layers.Activation("relu")(x)
+        x = layers.SeparableConv2D(size, 3, padding="same")(x)
+        x = layers.BatchNormalization()(x)
+
+        x = layers.MaxPooling2D(3, strides=2, padding="same")(x)
+
+        # Project residual
+        residual = layers.Conv2D(size, 1, strides=2, padding="same")(
+            previous_block_activation
+        )
+        x = layers.add([x, residual])  # Add back residual
+        previous_block_activation = x  # Set aside next residual
+
+    x = layers.SeparableConv2D(1024, 3, padding="same")(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation("relu")(x)
+
+    x = layers.GlobalAveragePooling2D()(x)
+
+    activation = "softmax"
+    units = NUM_CLASSES
+
+    x = layers.Dropout(0.5)(x)
+    outputs = layers.Dense(units, activation=activation)(x)
+    model = keras.Model(inputs, outputs)
+
+    return model
+
+
+if __name__ == "__main__":
+    model = build_my_model((64, 64, 3), NUM_CLASSES=10)
+    model.summary()
+
+    print("next model summary")
+    model_keras_tut = make_model((64, 64, 3), NUM_CLASSES=10)
+    model_keras_tut.summary()
---
+++ src/my_Classification_NN/main.py
@@ -0,0 +1,252 @@
+"""
+Main script, HAVELKA, 15/01/2021
+"""
+
+from core.nn_my_model import build_my_model
+from core.create_dataset import generate_dataset, generate_test_dataset
+
+import matplotlib.pyplot as plt
+from PIL import ImageFile
+
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+import os
+import matplotlib.pyplot as plt
+from pathlib import Path
+
+"""
+variables
+"""
+# not changable variables
+# fix bug
+ImageFile.LOAD_TRUNCATED_IMAGES = True
+# path to train folder
+train_folder_path = Path("data/Dataset_try/")
+# path for saving model checpoints
+model_checkpoint_path = './results/model_save/my_cnn_model.h5'
+# path for saving weights checkpoints
+weights_checkpoint_path = './results/training_checkpoints/weights.{epoch:02d}.ckpt'
+CLASS_NAMES = ['0', '1', '2']
+
+
+def train(num_classes, image_shape, EPOCHS=50):
+    """
+    train
+    - create dataset
+    - define model
+    - compile model
+    - fit model (train model)
+
+    :param num_classes: number of classes to be trained
+    :param image_shape: format (100, 100, 3)
+    :param EPOCHS: number of epochs
+
+    :return trained_model: keras model variable
+    """
+    # create datasets
+    train_ds, val_ds = generate_dataset(train_folder_path, image_size=image_shape[:-1])
+
+    # create model
+    model = build_my_model(image_shape, num_classes)
+
+    # define checkpoint - for saving best model
+    checkpoint_callbacks = keras.callbacks.ModelCheckpoint(
+        filepath=model_checkpoint_path,
+        save_best_only=True,
+        monitor='val_accuracy',
+        mode='max',
+        save_weights_only=False)
+
+    # define compiler
+    model.compile(
+        optimizer='adam',
+        loss=keras.losses.SparseCategoricalCrossentropy(),
+        metrics=["accuracy"])
+
+    # train it
+    model.fit(
+        train_ds,
+        epochs=EPOCHS,
+        validation_data=val_ds,
+        callbacks=[checkpoint_callbacks]
+    )
+
+    return model
+
+
+def transfer_train(num_classes, image_shape, path_to_model, path_to_save_model, EPOCHS=20, BATCH_SIZE=32):
+    """
+    :param num_classes: number of classes to be trained
+    :param image_shape: format (100, 100, 3)
+    :param path_to_model: path for loading pre-trained model
+    :param path_to_save_model: path for saving model after transfer learning
+    :param EPOCHS: number of epochs
+
+    :return trained_model: keras model variable
+
+    transfer_train
+    - create dataset
+    - load pre-trained model
+    - freeze model
+    - create new model on top
+    - train top layer
+    - finetuning - low lr, train entire model
+    - fit model (train model)
+    """
+    # FIRST STEP
+
+    train_ds, val_ds = generate_dataset(train_folder_path, image_size=image_shape[:-1], batch_size=BATCH_SIZE)
+
+    model_pre = keras.models.load_model(path_to_model)  # load pre-trained model
+    model_pre.pop()  # remove last layer
+
+    model_pre.trainable = False  # freeze model
+
+    inputs = keras.Input(shape=image_shape)  # define input
+    x = model_pre(inputs, training=False)  # pre-trained model
+    outputs = keras.layers.Dense(num_classes)(x)  # new output layer
+    model = keras.Model(inputs, outputs)  # create new model
+    model.summary()
+
+    # define checkpoint - for saving best model
+    checkpoint_callbacks = keras.callbacks.ModelCheckpoint(
+        filepath=path_to_save_model,
+        save_best_only=True,
+        monitor='val_accuracy',
+        mode='max',
+        save_weights_only=False)
+
+    # define compiler
+    model.compile(
+        optimizer='adam',
+        loss=keras.losses.SparseCategoricalCrossentropy(),
+        metrics=["accuracy"])
+
+    # train it
+    model.fit(
+        train_ds,
+        epochs=EPOCHS,
+        validation_data=val_ds,
+        callbacks=[checkpoint_callbacks]
+    )
+
+    # SECOND STEP - FINE TUNING - optional
+    # :TODO fine tunning
+
+    """
+    model_new = keras.Sequential()  # create new model
+    for layer in model_pre.layers[:-1]:  # go through until last layer
+        model_new.add(layer)
+
+    inputs = keras.Input(shape=image_shape)  # define input
+    x = model_new(inputs, training=False)
+    x = keras.layers.GlobalAveragePooling2D()(x)
+    outputs = keras.layers.Dense(num_classes)(x)  # new output layer
+    model = keras.Model(inputs, outputs)
+    """
+    return model
+
+
+def predict_img_loop(model, path_read, image_size=(32, 32)):
+    # go trough all folders in the path
+    for fo_number, folder in enumerate(os.listdir(path_read)):
+        print("new folder")
+        for fi_number, filename in enumerate(os.listdir(path_read+'\\'+folder)):
+            if fi_number > 20:
+                break
+            else:
+                img_path = path_read+str(folder)+'\\'+str(filename)
+                img = keras.preprocessing.image.load_img(
+                    img_path,
+                    target_size=image_size,
+                    color_mode="rgb",
+                    grayscale=False
+                )
+
+                img_array = keras.preprocessing.image.img_to_array(img)
+
+                tf_img_array = tf.expand_dims(img_array, 0)  # Create batch axis
+
+                predictions = model.predict(tf_img_array)
+                result = CLASS_NAMES_SVHN[predictions.argmax(axis=1)[0]]
+
+                print(
+                    "Predictions: ", predictions
+                )
+
+                plt.imshow(img_array/255.0)
+                plt.title(result)
+                plt.show()
+
+                print("Labels: ", result)
+
+
+def predict_image(model, PATH_TO_IMG, image_size):
+    img = keras.preprocessing.image.load_img(
+        PATH_TO_IMG,
+        target_size=image_size,
+        color_mode="rgb",
+        grayscale=False
+    )
+
+    img_array = keras.preprocessing.image.img_to_array(img)
+
+    tf_img_array = tf.expand_dims(img_array, 0)  # Create batch axis
+
+    predictions = model.predict(tf_img_array)
+    result = CLASS_NAMES[predictions.argmax(axis=1)[0]]
+
+    print(
+        "Predictions: ", predictions
+    )
+
+    plt.imshow(img_array/255.0)
+    plt.title(result)
+    plt.show()
+
+    print("Labels: ", result)
+
+
+if __name__ == "__main__":
+    """
+    variables
+    """
+    NUM_CLASSES = 3  # number of classes
+    EPOCHS = 30  # epochs count
+    BATCH_SIZE = 10  # batch size
+    image_shape = (32, 32, 3)  # input image shape
+
+    path_to_model_tfl = 'data/models/svhn_best_cnn.h5'  # saved model path, for loading saved models
+    path_to_save_model_tfl = 'results/model_save/transfer/my_transfer_model.h5'
+
+    path_to_trained_model = 'results/model_save/from_scratch/'  # saved model path, for loading saved models
+
+    path_to_dataset = 'data/Dataset_try/'  # main dataset path
+    path_to_img = Path("data/test/img_test/5_frame_011086.jpeg")
+    TRAIN = False  # = True --> train neural network, = False --> load model or weights and eveluate network
+    TRANSFER = True  # = True --> treansfer learning, use pre-trained model, = False --> train own model
+    LOAD_MODEL = True  # = True --> load model, = False --> load weights
+
+    if TRAIN is True:
+        print("Training is starting ...")
+        if TRANSFER is True:
+            transfer_train(
+                NUM_CLASSES,
+                image_shape,
+                path_to_model_tfl,
+                path_to_save_model_tfl,
+                EPOCHS=EPOCHS,
+                BATCH_SIZE=BATCH_SIZE
+            )
+        else:
+            train(NUM_CLASSES, image_shape, EPOCHS=EPOCHS)
+    else:
+        if LOAD_MODEL is True:
+            print("Load model and start predictions on images ...")
+            model = keras.models.load_model(path_to_save_model_tfl)
+            predict_image(model, path_to_img, image_shape[:-1])
+            print("Prediction is done.")
+        else:
+            model_old = build_my_model(image_shape, NUM_CLASSES)
+            # TODO: use function for loading weights to model
